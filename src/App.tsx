ccb1589813306f910e11c2ee9debbd07933a223e
ErezEfrat <ereze2@gmail.com>
2021-02-08 23:25:35 +0200

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+Amplify.configure(awsExports);
+
 function App() {
   return (
     <div className="App">
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
